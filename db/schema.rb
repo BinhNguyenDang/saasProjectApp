4076f8c3d64b8b33f1b9a5e570238357c6e545e6
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-05-13 00:21:20 -0400

    Add/generate Members resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_035640) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_041339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_035640) do
     t.string "domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "account_id", null: false
+    t.json "roles", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_members_on_account_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -42,5 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_035640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "members", "accounts"
+  add_foreign_key "members", "users"
   add_foreign_key "projects", "accounts"
 end
