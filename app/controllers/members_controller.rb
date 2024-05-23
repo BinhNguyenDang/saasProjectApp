85034566aeb9439783ab060431297ca665bf18c7
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-05-23 11:19:12 +0700

    Add Authorized controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,9 +5,16 @@ class MembersController < ApplicationController
         @member = @current_account.members 
     end
 
-    private 
+    def invite 
+        email = params[:email]
+        return redirect_to account_members_path(@current_account), alert: "No email provided" if email.blank?
 
-    def set_current_account
-        @current_account = Account.find(params[:account_id])
+        user = User.find_by(email:) || User.invite!({ email: }, current_user ) # current_user will be set as invited_by
+
+        return redirect_to account_members_path(@current_account), alert: "Email invalid" unless user.valid? 
+
+        user.members.find_or_create_by(account: @current_account, roles: {admin: false, editor: true})
+        # binding.b
+        redirect_to account_members_path(@current_account), notice: "#{email} invited"
     end
 end
