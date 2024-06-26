9ba6e296e101d60ff3bda7570b68e518bc8efb16
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-06-26 16:28:12 +0700

    Define Admin role in members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -14,30 +14,31 @@ class MembersController < ApplicationController
       email = params[:email]
       # Redirect if no email is provided
       return redirect_to project_members_path(@current_project), alert: "No email provided" if email.blank?
-  
-      user = User.find_by(email: email)
-      if user.nil?
-        # Invite the user if they do not exist
-        user = User.invite!({ email: email }, current_user) # current_user will be set as invited_by
-        # Redirect if the email is invalid
-        return redirect_to project_members_path(@current_project), alert: "Email Invalid" unless user.persisted?
-      end
+
+      if current_user.admin? #check if the current user is an admin 
+        user = User.find_by(email: email)
+        if user.nil?
+
+          # Invite the user if they do not exist
+          user = User.invite!({ email: email }, current_user) # current_user will be set as invited_by
+          # Redirect if the email is invalid
+          return redirect_to project_members_path(@current_project), alert: "Email Invalid" unless user.persisted?
+        end
   
       # Check if the user is already a member of the project
-      if user.members.find_by(project: @current_project).nil?
-        # Add the user as a member with specified roles
-        user.members.create(project: @current_project, roles: { admin: false, editor: true })
-        redirect_to project_members_path(@current_project), notice: "#{email} invited!"
-      else
-        redirect_to project_members_path(@current_project), alert: "#{email} is already a member!"
+        if user.members.find_by(project: @current_project).nil?
+          # Add the user as a member with specified roles
+          user.members.create(project: @current_project, roles: { admin: false, editor: true })
+          redirect_to project_members_path(@current_project), notice: "#{email} invited!"
+        else
+          redirect_to project_members_path(@current_project), alert: "#{email} is already a member!"
+        end
+      else  
+        redirect_to project_members_path(@current_project), alert: "You are not authorized to invite members!"
       end
     end
+end
   
-    private
+
   
-    # Set the current project based on parameters
-    def set_current_project
-      @current_project = Project.find(params[:project_id])
-    end
-  end
-  
+    
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   has_many :members
   has_many :projects, through: :members
   has_many :accounts, foreign_key: 'creator_id'
+
+  def admin?
+    # Retrieve the first member from the collection of members associated with the current object.
+    member = self.members.first
+    # Check if the member is present and if they have admin privileges.
+    member.present? && member.admin?
+  end
 end
