a29920e9c2fab9b38f658b9d7fa41185c46d8363
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-07-08 17:53:39 +0700

    Stripe initializers and other setup for stripe

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,5 +12,7 @@ class Account < ApplicationRecord
     # The association uses the creator_id column in the accounts table to find the corresponding user.
     belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
     has_many :projects, dependent: :destroy
+    has_one :payment
+    accepts_nested_attributes_for :payment
   end
   
