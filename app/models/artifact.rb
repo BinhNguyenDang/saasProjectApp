64b2bc46ff15fa9403cb53ccbe066b95154a08fa
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-07-08 15:09:00 +0700

    Update Artifact model & controller for add Active storage

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,19 +1,20 @@
 class Artifact < ApplicationRecord
-  attr_accessor :upload
   belongs_to :project
 
+  has_one_attached :file
+
   MAX_FILESIZE = 10.megabytes
-  validates_presence_of :name, :upload
+  validates_presence_of :name, :file
   validates_uniqueness_of :name
 
   # custom validation for file size
-  validate :upload_file_size
+  validate :uploaded_file_size
 
   private
 
-  def upload_file_size
-    if upload
-      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}MB") unless upload.size <= self.class::MAX_FILESIZE
+  def uploaded_file_size
+    if file.attached? && file.blob.byte_size > self.class::MAX_FILESIZE
+      errors.add(:file, "File size should be less than #{self.class::MAX_FILESIZE / 1.megabyte}MB")
     end
   end
 end
